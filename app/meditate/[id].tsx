84e59d97202e5da89be877615b7a0083a69a26e7
Praise <praisetoyo@gmail.com>
2025-02-08 23:48:24 +0100

    feat: add audio playing during meditation

--- app/meditate/[id].tsx
+++ app/meditate/[id].tsx
@@ -6,11 +6,15 @@ import AppGradient from "@/components/AppGradient";
 import {router, useLocalSearchParams} from "expo-router";
 import {AntDesign} from "@expo/vector-icons";
 import CustomButton from "@/components/CustomButton";
+import {Audio} from "expo-av"
+import {MEDITATION_DATA, AUDIO_FILES} from "@/constants/meditationData";
 
 const Meditate = () => {
     const {id} = useLocalSearchParams();
     const [secondsRemaining, setSecondsRemaining] = useState(10)
     const [isMeditating, setIsMeditating] = useState(false)
+    const [audioSound, setSound] = useState<Audio.Sound>()
+    const [isPlayingAudio, setPlayingAudio] = useState(false)
 
     useEffect(() => {
         let timerId: NodeJS.Timeout;
@@ -33,6 +37,41 @@ const Meditate = () => {
         }
     }, [secondsRemaining, isMeditating]);
 
+    const toggleMeditationSessionStatus = async () => {
+        if (secondsRemaining === 0) setSecondsRemaining(10)
+
+        setIsMeditating(!isMeditating)
+
+        await toggleSound()
+    }
+
+    const toggleSound = async() => {
+        const sound = audioSound ? audioSound : await initializeSound()
+
+        const status = await sound?.getStatusAsync();
+
+        if (status?.isLoaded && !isPlayingAudio){
+            await sound.playAsync()
+            setPlayingAudio(true)
+        }
+        else{
+            await sound.pauseAsync()
+            setPlayingAudio(false)
+        }
+
+    }
+
+    const initializeSound = async () => {
+        const audioFileName = MEDITATION_DATA[Number(id) - 1].audio;
+
+        const {sound} = await Audio.Sound.createAsync(
+            AUDIO_FILES[audioFileName]
+        )
+
+        setSound(sound)
+        return sound
+    }
+
     // Format the time left to ensure two digits are displayed
     const formattedTimeMinutes = String(Math.floor(secondsRemaining / 60)).padStart(2, "0");
     const formattedTimeSeconds = String(secondsRemaining % 60).padStart(2, "0");
@@ -46,12 +85,15 @@ const Meditate = () => {
 
                     <View className='flex-1 justify-center'>
                         <View className='mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center'>
-                            <Text className='text-4xl text-blue-800 font-rmono'>{formattedTimeMinutes}:{formattedTimeSeconds}</Text>
+                            <Text
+                                className='text-4xl text-blue-800 font-rmono'>{formattedTimeMinutes}:{formattedTimeSeconds}</Text>
                         </View>
                     </View>
 
-                    <View className='mb-5'><CustomButton onPress={() => setIsMeditating(true)}
-                                                         title={"Start Meditation"}/></View>
+                    <View className='mb-5'>
+                        <CustomButton onPress={toggleMeditationSessionStatus}
+                                      title={"Start Meditation"}/>
+                    </View>
                 </AppGradient>
             </ImageBackground>
         </View>
